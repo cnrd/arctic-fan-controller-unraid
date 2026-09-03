78b196cf2479a57da8399417e58a179067763b69
Conrad Juhl Andersen <git@cnrd.me>
2026-09-03 18:36:51 +0200

    Add DMA annotation compatibility header

---
+++ driver/compat.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef ARCTIC_FAN_CONTROLLER_COMPAT_H
+#define ARCTIC_FAN_CONTROLLER_COMPAT_H
+
+#ifndef __dma_from_device_group_begin
+#define __dma_from_device_group_begin()
+#endif
+
+#ifndef __dma_from_device_group_end
+#define __dma_from_device_group_end()
+#endif
+
+#endif
